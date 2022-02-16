331c9d3446005704d302a6b1cb4f27519e675f47
NottSr <leonardo.br4@hotmail.com>
2022-02-15 17:36:21 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j = 0;
+	char l[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (j <= 10)
+	{
+		int i = 0;
+
+		while (i <= 26)
+		{
+			_putchar(l[i]);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
