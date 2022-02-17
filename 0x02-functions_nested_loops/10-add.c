0918b435cb5018f22c8c4cbc779d9452a44a5e60
NottSr <leonardo.br4@hotmail.com>
2022-02-16 19:00:02 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - check the code
+ * @a: first int
+ * @b: second int
+ * Return: res, that is the result from a + b
+ */
+int add(int a, int b)
+{
+	int res = 0;
+
+	res = a + b;
+	return (res);
+}
