a836f36cfdad836832675e7a9ed6eb216649cf5e
NottSr <leonardo.br4@hotmail.com>
2022-02-26 02:10:56 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - Entry point
+ * @dest: first variable checked
+ * @src: sec variable checked
+ * @n: n bytes
+ * Return: Always dest (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, dlen;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	dlen = i;
+	i = 0;
+	while ( i < n && src[i])
+	{
+		dest[dlen + i] = src[i];
+		i++;
+	}
+	return (dest);
+}
