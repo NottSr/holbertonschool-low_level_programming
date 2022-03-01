fe7ea7ebb7b610446b3305cca13072c307d06cf2
NottSr <leonardo.br4@hotmail.com>
2022-02-28 18:06:05 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - Entry point
+ * @s: Array checked
+ * @b: Char checked
+ * @n: Number of bytes to be re-write
+ * Return: Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i <= n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory was made in order to accomplish the tasks for 0x07. C - Even more pointers, arrays and strings project
