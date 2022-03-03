96ad27d2c372a5a6182a053c06bc39ad7d60d263
NottSr <leonardo.br4@hotmail.com>
2022-03-02 16:53:17 -0800

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * odd_count - Entry point
+ * @i: int checked
+ * @j: int checked to multiply
+ * Return: (Success)
+ */
+int sqrt_count(int i, int j)
+{
+	if (j * j == i)
+	{
+		return (j);
+	}
+	if (j * j > i)
+	{
+		return (-1);
+	}
+	return (sqrt_count(i, j + 1));
+}
+/**
+ * sqrt_recursion - Entry point
+ * @n: int checked
+ * Return: (Success)
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_count(n, 1));
+}
