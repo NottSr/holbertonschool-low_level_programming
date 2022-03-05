4343b2add2fa5bd4ee15111e7b359f92f604c1da
NottSr <leonardo.br4@hotmail.com>
2022-03-04 21:01:01 -0800

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * my_name - Entry point
+ * @argc:
+ * @argv:
+ * Return: Always 0 (Success)
+ */
 int my_name(int argc, char* argv[])
 {
 	int i = 0;
