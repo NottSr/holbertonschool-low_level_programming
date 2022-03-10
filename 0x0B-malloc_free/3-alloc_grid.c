deda976c526bc63bb3555d66658ba1137a300edb
NottSr <leonardo.br4@hotmail.com>
2022-03-09 19:25:30 -0800

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Entry point
+ * @width: the width of the grid
+ * @height: height of the grid
+ * Return: Always agrid (Success)
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j, **agrid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	agrid = malloc(sizeof(*agrid) * height);
+
+	if (agrid == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < height)
+	{
+		j = 0;
+		*(agrid + i) = malloc(sizeof(*agrid) * width);
+		if (*(agrid + i) == NULL)
+		{
+			i = 0;
+			while (i < height)
+			{
+				free(*(agrid + i));
+				i++;
+			}
+			free(agrid);
+			return (NULL);
+		}
+		while (j < width)
+		{
+			agrid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+
+	return (agrid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,3 +7,5 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 
 unsigned int _counter(char *ar, unsigned int i);
+
+int **alloc_grid(int width, int height);
