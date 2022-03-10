ab8a979b8c4129e87a7e8ede15980879b97b2bdc
NottSr <leonardo.br4@hotmail.com>
2022-03-09 17:38:23 -0800

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,30 +12,15 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, size = 0, dlen_s1 = 0, dlen_s2 = 0;
 	char *s3;
 
-	while (s1[i])
-	{
-		i++;
-	}
-
-	dlen_s1 = i;
-	i = 0;
-
-	while (s2[i])
-        {
-                i++;
-        }
-
-        dlen_s2 = i;
-	i = 0;
-	size = dlen_s1 + dlen_s2;
+	dlen_s1 = _counter(s1, i);
+	dlen_s2 = _counter(s2, i);
+	size = dlen_s1 + dlen_s2 + 1;
 	s3 = malloc(sizeof(char) * size); /*asign the size*/
-
 	if (s3 == NULL)
 	{
 		return (NULL);
 	}
-
-	while (i < size)
+	while (i <= size)
 	{
 		if (i < dlen_s1)
 		{
@@ -48,7 +33,21 @@ char *str_concat(char *s1, char *s2)
 		}
 		i++;
 	}
-
-	*(s3 + i) = '\0';
+	*(s3 + (size + 1)) = '\0';
 	return (s3);
+}
+
+/**
+ * _counter - Count the bytes of the array
+ * @ar: string checked
+ * @i: length of the array
+ * Return: i (Success)
+ */
+unsigned int _counter(char *ar, unsigned int i)
+{
+	if (*ar == 0)
+	{
+		return (i);
+	}
+	return (_counter(ar + 1, i + 1));
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,3 +5,5 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 
 char *str_concat(char *s1, char *s2);
+
+unsigned int _counter(char *ar, unsigned int i);
