a0482de3da6d97e8e752905a62b957ffa8fce6e2
NottSr <leonardo.br4@hotmail.com>
2022-03-14 20:53:05 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+/*
+ * struct dog - Structure with dog info
+ * @name: char with dog name
+ * @age: float with dog age
+ * @owner: char with dog's owner name
+ */
 struct dog
 {
+	/*Description: This structure works to declare dog info with var*/
 	char *name;
-	double age;
+	float age;
 	char *owner;
 };
+#endif
