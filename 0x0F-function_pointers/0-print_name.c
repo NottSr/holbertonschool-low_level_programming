d37589c457c062c4cc6e35ec43edec63ffeebfaf
NottSr <leonardo.br4@hotmail.com>
2022-03-15 23:49:12 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - Function that calls functions
+ * @name: string checked
+ * @f: function checked
+ * Return: void (Success)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _FPTR_
+#define _FPTR_
+void print_name(char *name, void (*f)(char *));
+#endif
