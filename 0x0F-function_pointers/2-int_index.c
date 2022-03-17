5cfbcdb84b136f0ef727c96fac85c5b60effdecb
NottSr <leonardo.br4@hotmail.com>
2022-03-16 21:24:33 -0700

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - Function that calls functions
+ * @array: array checked
+ * @size: size of array checked
+ * @cmp: fuction checked
+ * Return: ind (Success)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while(i < size)
+	{
+		if(cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
