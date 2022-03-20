d65258365c7fd1aa602d99fb5e999bb99305f646
NottSr <leonardo.br4@hotmail.com>
2022-03-19 18:25:12 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,15 +9,79 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0, n = 5;
+	unsigned int i = 0, j;
+	char *separator = "";
 	va_list pall;
-	get_ty gty[] = {
-		{"c", char},
-		{"i", int},
-		{"f", float},
-		{"s", char *}
+	get_ty get_type[] = {
+		{"c", pr_char},
+		{"i", pr_int},
+		{"f", pr_float},
+		{"s", pr_str}
 	};
-	va_start(pall, n);
-	printf("%s", format);
-	va_end pall;
+	va_start(pall, format);
+	while (format[i] != '\0' && format != NULL)
+	{
+		j = 0;
+		while (j <= 3)
+		{
+			if (format[i] == *get_type[j].type)
+			{
+				get_type[j].f(separator, pall);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(pall);
+}
+/**
+ * pr_char - print char var
+ * @separator: separator
+ * @pall: variadic args
+ * Return: (Success)
+ */
+void pr_char(char *separator, va_list pall)
+{
+	printf("%s%c", separator, va_arg(pall, int));
+}
+/**
+ * pr_int - print int var
+ * @separator: separator
+ * @pall: variadic args
+ * Return: (Success)
+ */
+void pr_int(char *separator, va_list pall)
+{
+	printf("%s%d", separator, va_arg(pall, int));
+}
+/**
+ * pr_float - print float var
+ * @separator: separator
+ * @pall: variadic args
+ * Return: (Success)
+ */
+void pr_float(char *separator, va_list pall)
+{
+	printf("%s%f", separator, va_arg(pall, double));
+}
+/**
+ * pr_str - print char * var
+ * @separator: separator
+ * @pall: variadic args
+ * Return: (Success)
+ */
+void pr_str(char *separator, va_list pall)
+{
+	char *str = va_arg(pall, char *);
+
+	if (str == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+	}
+	else
+	{
+		printf("%s%s", separator, str);
+	}
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,23 @@
 #ifndef _VFUNC_
 #define _VFUNC_
-struct get_type
+#include <stdarg.h>
+/**
+ * struct get_type - struct to get type
+ * @type: type
+ * @f: function
+ */
+typedef struct get_type
 {
 	char *type;
-	char (*f)(char *separator, va_list pall);
-}get_ty;
+	void (*f)(char *separator, va_list pall);
+} get_ty;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void pr_char(char *separator, va_list pall);
+void pr_int(char *separator, va_list pall);
+void pr_float(char *separator, va_list pall);
+void pr_str(char *separator, va_list pall);
 #endif
