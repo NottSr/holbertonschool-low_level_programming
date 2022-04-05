1ada2f1eef0d935d51d152b7133939c972702876
NottSr <leonardo.br4@hotmail.com>
2022-04-04 23:33:43 -0500

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * clear_bit - clear bit asked
+ * @n: number checked
+ * @index: index
+ * Return: -1 or 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
