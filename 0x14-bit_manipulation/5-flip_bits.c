be59fc440a1b87b1ba61eb73f18e8f69373b0a5b
NottSr <leonardo.br4@hotmail.com>
2022-04-04 23:55:43 -0500

    101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * flip_bits - flip bits asked
+ * @n: number checked
+ * @index: index
+ * Return: Always 0.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int aux = 0, fbits = 0;
+
+	aux = n ^ m;
+	while(aux)
+	{
+		if (aux & 1)
+			fbits++;
+
+		aux >>= 1;
+	}
+	return (fbits);
+}
