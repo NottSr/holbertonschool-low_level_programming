add8421d73afb2da83922d39abe9235346ea766d
NottSr <leonardo.br4@hotmail.com>
2022-05-12 13:25:20 -0500

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -36,6 +36,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 					(new->next)->prev = new;
 				return (new);
 			}
+
+			if(i + 1 == idx && current->next != NULL)
+			{
+				new = add_dnodeint_end(h, n);
+				return (new);
+			}
 			current = current->next;
 			i++;
 		}
