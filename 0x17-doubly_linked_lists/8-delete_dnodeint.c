fc846f8f0f4939e0c1660821416407e871d923fd
NottSr <leonardo.br4@hotmail.com>
2022-05-12 23:02:13 -0500

    8. Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,7 +7,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *aux = *head;
+	dlistint_t *aux, *node = *head;
 	unsigned int i = 0;
 
 	if (!*head || !head)
@@ -15,24 +15,30 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(aux);
+		if ((*head)->next != NULL)
+		{
+			(node->next)->prev = NULL;
+			*head = node->next;
+		}
+		else
+			*head = NULL;
+		free(node);
 		return (1);
 	}
-	aux = *head;
-	while (aux)
+	while (i < index - 1 && node != NULL)
 	{
-		if (i == index)
-		{
-			aux->next->prev = aux->prev;
-			aux->prev->next = aux->next;
-			free(aux);
-			return (1);
-		}
-		aux = aux->next;
+		node = node->next;
 		i++;
 	}
-	return (-1);
+	if (node == NULL || node->next == NULL)
+		return (-1);
+
+	if ((node->next)->next != NULL)
+		((node->next)->next)->prev = node;
+
+	aux = (node->next)->next;
+	free(node->next);
+	node->next = aux;
+
+	return (1);
 }
