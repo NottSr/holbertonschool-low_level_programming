7d9da64b1ae1c384e4c94bea58317e67599f9d89
NottSr <leonardo.br4@hotmail.com>
2022-05-16 13:22:20 -0500

    3. Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * add - function that sums
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function that sub
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - function that mul
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function that div
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function that div
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
