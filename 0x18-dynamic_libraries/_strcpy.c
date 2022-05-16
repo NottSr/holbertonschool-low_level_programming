371439e8d987a3b4e8cdb9734b2240f1fc6a13a6
NottSr <leonardo.br4@hotmail.com>
2022-05-16 12:39:03 -0500

    fix 0. A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/_strcpy.c
+++ 0x18-dynamic_libraries/_strcpy.c
@@ -5,4 +5,16 @@
  * @src: Surce
  * Return: Success
  */
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
