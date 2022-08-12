edeabdbdd84def7555a0888e860be714df6365fe
NottSr <leonardo.br4@hotmail.com>
2022-08-12 11:58:01 -0500

    Task 102 done

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,25 +17,27 @@ int interpolation_search(int *array, size_t size, int value)
 		return (-1);
 
 	while (low <= high)
-    {
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	{
+		pos = low + (
+			((double)(high - low) / (array[high] - array[low])) * (value - array[low])
+			);
 
-        if (array[high] < value)
-            break;
+		if (array[high] < value)
+			break;
 
-        printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] == value)
-            return pos;
+		if (array[pos] == value)
+			return (pos);
 
-        if (array[pos] < value)
-            low = pos + 1;
+		if (array[pos] < value)
+			low = pos + 1;
 
-        if (array[pos] > value)
-            high = pos - 1;
-    }
+		if (array[pos] > value)
+			high = pos - 1;
+	}
 
-    printf("Value checked array[%ld] is out of range\n", pos);
+	printf("Value checked array[%ld] is out of range\n", pos);
 
 	return (-1);
 }
